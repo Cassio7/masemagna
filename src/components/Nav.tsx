0a8b739453b83a7c8e4ab8876b53d9026bcb8a59
Cassio7 <acassieri@gmail.com>
2025-01-31 00:17:29 +0100

    navbar responsive complete

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,68 @@
-import { useLocation } from "@solidjs/router";
+import { createSignal } from "solid-js";
 
 export default function Nav() {
-  const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+  const [isOpen, setIsOpen] = createSignal(false); // Gestione dello stato del menu
+
   return (
-    <div class="navbar color_back">
+    <nav class="navbar color_back relative">
+      {/* Logo */}
       <div class="flex-1 ml-10">
         <a href="/" class="title">
           MaseMagna
         </a>
       </div>
-      <div class="flex-none">
-        <ul class="menu menu-horizontal px-1 color_text">
-          <li>
-            <a href="explore">Esplora</a>
-          </li>
-          <li>
-            <a href="about">Contatti</a>
-          </li>
-        </ul>
+
+      {/* Hamburger Menu (per dispositivi piccoli) */}
+      <div class="flex-none lg:hidden mr-10">
+        <button
+          class="btn btn-ghost"
+          onClick={() => setIsOpen(!isOpen())} // Cambia lo stato
+        >
+          {/* Icona hamburger */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-[#14151c]"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
       </div>
-    </div>
+
+      {/* Link per schermi grandi */}
+      <div class="hidden lg:flex flex-none mr-10">
+        <a class="normal mr-5" href="/explore">
+          Esplora
+        </a>
+        <a class="normal" href="/about">
+          Contatti
+        </a>
+      </div>
+
+      {/* Menu Mobile */}
+      {isOpen() && (
+        <div class="absolute top-full left-0 w-full bg-[#ffe9df] shadow-lg lg:hidden">
+          <ul class="menu menu-vertical p-4">
+            <li class="mb-2">
+              <a class="normal" href="/explore">
+                Esplora
+              </a>
+            </li>
+            <li>
+              <a class="normal" href="/about">
+                Contatti
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
   );
 }
