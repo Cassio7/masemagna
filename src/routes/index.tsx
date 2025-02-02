bb27c1c9893234434c2b81f8d859da172dff7d07
Cassio7 <acassieri@gmail.com>
2025-02-03 00:11:20 +0100

    add card and img, need to fix img

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import Card from "~/components/card/Card";
+
 export default function Home() {
   return (
     <main class="text-center mx-auto">
@@ -10,22 +12,52 @@ export default function Home() {
           </div>
           <div>
             <h1 class="title-main mt-28" style={{ "font-size": "13vw" }}>
+              <div class="-mb-40">
+                <img
+                  class="img-flip"
+                  src="assets/img/pig.png"
+                  alt="logo"
+                  style={{ height: "11vw", width: "11vw" }}
+                />
+              </div>
               Masemagna
             </h1>
           </div>
           <div>
-            <h1 class="normal_sottomain mt-14">
+            <h1 class="normal_sottomain mt-14 mb-48">
               Fidati di noi per trovare sempre la qualità e il risparmio <br />i
               soldi en pochi e i tribbli en tanti
             </h1>
           </div>
         </div>
       </div>
-      <div class="flex flex-row justify-center">
-        <div>card</div>
-        <div>card</div>
-        <div>card</div>
+      <div class="flex flex-wrap justify-evenly py-28">
+        <div>
+          <Card
+            title="semplicita"
+            image="assets/img/ramen.png"
+            img_alt="ramen"
+            description="dededed"
+          />
+        </div>
+        <div>
+          <Card
+            title="Cibo Etnico"
+            image="assets/img/ramen.png"
+            img_alt="ramen"
+            description="Amo tantissimo il cibo etnico"
+          />
+        </div>
+        <div>
+          <Card
+            title="TESTO"
+            image="assets/img/ramen.png"
+            img_alt="ramen"
+            description="dededed"
+          />
+        </div>
       </div>
+      <div class="justify-center"></div>
     </main>
   );
 }
