e4104e3f35683875d39baff7f9d4dd012e82a13d
cassio7 <acassieri@gmail.com>
2025-08-29 18:29:44 +0200

    feat: rating on posts

--- src/lib/interfaces/post.interface.ts
+++ src/lib/interfaces/post.interface.ts
@@ -1,10 +1,12 @@
 import type { Base } from "@/lib/interfaces/base.interface";
 import type { Author } from "./author.interface";
 import type { Category } from "./category.interface";
+import type { Rating } from "./rating.interface";
 
 export interface Post extends Base {
   name: string;
   description: string;
   author: Author;
   category: Category;
+  rating: Rating[];
 }
