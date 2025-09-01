4cacee884d28d4bc81b3ab86175d09e7034506e0
cassio7 <acassieri@gmail.com>
2025-09-01 23:22:09 +0200

    feat: netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://masemagna.netlify.app',
+  site: "https://masemagna.netlify.app",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  output: "static",
+  adapter: netlify(),
 });
